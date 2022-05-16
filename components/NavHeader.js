1653a9d4a4ad22cbffa2099a3916ea1ecf408ca1
AugustusMadden <gusmadden@gmail.com>
2022-05-15 21:39:08 -0400

    Added initial react syntax

--- components/NavHeader.js
+++ components/NavHeader.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
 export default function NavHeader(params) {
-    return ();
+    return (
+        <header>
+            <div id = 'masthead'>
+                <p class='site-head'>
+                    <a href="">
+                        
+                    </a>
+                </p>
+            </div>
+        </header>
+    );
 }
--- components/Pages/Portfolio.js
+++ components/Pages/Portfolio.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
 export default function Portfolio() {
-    return ();
+    return (
+        <div>
+        </div>
+    );
 }
