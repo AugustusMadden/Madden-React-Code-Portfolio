b8b7cdb1b2b032734f2df5d9b4eb51f6202f3583
AugustusMadden <gusmadden@gmail.com>
2022-05-17 21:23:54 -0400

    Debugging

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -5,17 +5,11 @@ export default function About() {
         <div>
             <h1>About Me</h1> 
             <p>
-            My name is Augustus Madden. I am a recent graduate of Drexel University's Westphal
-College of Media Arts and Design, having earned a Bachelor's degree in Animation and Visual
-Effects. 
+            My name is Augustus Madden. I am a recent graduate of Drexel University's Westphal College of Media Arts and Design, having earned a Bachelor's degree in Animation and Visual Effects. I've spent most of my life living in the Philadelphia Area. My main hobbies include cooking and avoiding interacting with any form of advertisement as often as physically possible.
             </p>
             <br></br>
             <p>
-            Profesional Information
-            I'm interested in all aspects of content creation, with a current focus on video and
-effects work. I have a strong interest in narrative and interactivity, and I'm looking for an
-opportunity to join a team of professionals interested in creating meaningful, compelling
-content.
+            I'm interested in all aspects of content creation, with a current focus on web design work. I have a strong interest in narrative and interactivity, and I'm looking for an opportunity to join a team of professionals interested in creating meaningful, compelling content.
             </p>
         </div>
         
