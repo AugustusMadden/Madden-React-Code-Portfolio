cb36c7f1d32c45b6a4cb4758937fe1704cb09860
AugustusMadden <gusmadden@gmail.com>
2023-03-07 15:47:57 -0500

    Nav cleanup

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,10 @@ import './Styles/Footer.css'
 export default function Footer() {
     return (
         <div className = "footer">
-            <p>Copyright © 2023 | All Rights Reserved</p>
+            <div>
+                <p>Copyright © 2023 | All Rights Reserved</p>
+            </div>
         </div>
+        
     );
 }
--- src/components/NavHeader.js
+++ src/components/NavHeader.js
@@ -5,43 +5,46 @@ import Resume from '../assets/Madden_Resume.pdf'
 export default function NavHeader({ currentPage, handlePageChange }) {
     return (
       <div className="navbar">
-        <ul className="nav nav-header">
-          <li className="nav-item">
-            <a
-              href="#portfolio"
-              onClick={() => handlePageChange('Portfolio')}
-              className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
-            >
-              Portfolio
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              href="#about"
-              onClick={() => handlePageChange('About')}
-              className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-            >
-              About
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              href={Resume}
-              className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-            >
-              Resume
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              href="#contact"
-              onClick={() => handlePageChange('Contact')}
-              className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-            >
-              Contact
-            </a>
-          </li>
-        </ul>
+        <div>
+          <ul className="nav nav-header">
+            <li className="nav-item">
+              <a
+                href="#portfolio"
+                onClick={() => handlePageChange('Portfolio')}
+                className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+              >
+                Portfolio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="#about"
+                onClick={() => handlePageChange('About')}
+                className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+              >
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href={Resume}
+                className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+              >
+                Resume
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="#contact"
+                onClick={() => handlePageChange('Contact')}
+                className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+        
       </div>
     );
 }
